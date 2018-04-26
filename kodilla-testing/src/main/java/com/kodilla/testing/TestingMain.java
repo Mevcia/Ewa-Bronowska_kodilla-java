1ced027addec349ab27c3f53c6291191b72cbd97
Mevcia <ewa.a.bronowska@gmail.com>
2018-04-26 14:14:19 +0200

    Add clases Calculator and SimpleUser, updated class TestingMain

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.print("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUserName();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(21,7);
+
+        if (addResult == 21 + 7) {
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator1 = new Calculator();
+
+        int substractResult = calculator1.substract(21,7);
+
+        if (substractResult == 21 - 7) {
+            System.out.println("Test Ok");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
