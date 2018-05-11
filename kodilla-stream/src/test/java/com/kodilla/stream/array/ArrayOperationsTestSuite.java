073e14fe6a775b52d4d2e2b6a486bfdda1494b5f
Mevcia <ewa.a.bronowska@gmail.com>
2018-05-11 19:21:50 +0200

    Add package array - ex. 7.5 IntSream, range()  and average()

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void shouldReturnAverage() {
+        //Given
+        int[] numbers = {1, 99, 3, 5, 44, 66, 7, 9, 6, 31, 5, 11, 8, 12, 19, 26, 2, 4, 70, 22};
+        ArrayOperator arrayOperator = new ArrayOperator();
+        //When
+        double average = arrayOperator.getAverage(numbers);
+        //Then
+        Assert.assertEquals(22.5, average, 0.0001);
+    }
+}
