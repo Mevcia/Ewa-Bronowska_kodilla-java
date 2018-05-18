e9011095682e943300890ad542e40d21d7d66ec0
Mevcia <ewa.a.bronowska@gmail.com>
2018-05-18 21:35:14 +0200

    Add modul 8.4 - example and excercise exceptions usage

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/index/VideoCollector.java
@@ -0,0 +1,12 @@
+package com.kodilla.exception.index;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoCollector {
+    public List<String> getCollection() {
+        List<String> videoList = new ArrayList<>();
+        videoList.add("Extra movie");
+        return videoList;
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,31 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+    private Map<String, Boolean> flightMap = new HashMap<>();
+
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+        flightMap.put("Eindhoven", true);
+        flightMap.put("Warsaw", true);
+        flightMap.put("Lodz", false);
+
+        if ((flightMap.get(flight.getDepartureAirport())) && (flightMap.get(flight.getArrivalAirport()))) {
+            System.out.println("You can flight from " + flight.getDepartureAirport() + " to " + flight.arrivalAirport);
+        } else {
+            throw new RouteNotFoundException("Flight not possible");
+        }
+    }
+    public static void main(String[] args) {
+        FlightFinder flightFinder = new FlightFinder();
+        Flight flight = new Flight("Eindhoven", "Lodz");
+        try {
+            flightFinder.findFilght(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("Flight not found");
+        } finally {
+            System.out.println("Want to find another flight?");
+        }
+    }
+}
