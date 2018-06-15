e5b935ed86497d3b4d4d7c546f9b7101fbeb1d03
Mevcia <ewa.a.bronowska@gmail.com>
2018-06-15 21:30:04 +0200

    Correct calculator

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -5,30 +5,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Calculator {
-    private double val;
     @Autowired
     private Display display;
 
     public double add(double a, double b) {
-        val = a + b;
-        display.displayValue(val);
-        return val;
+        double result = a + b;
+        display.displayValue(result);
+        return result;
     }
     public double sub(double a, double b) {
-        val = a - b;
-        display.displayValue(val);
-        return val;
+        double result = a - b;
+        display.displayValue(result);
+        return result;
     }
     public double mul(double a, double b) {
-        val = a * b;
-        display.displayValue(val);
-        return val;
+        double result = a * b;
+        display.displayValue(result);
+        return result;
     }
     public double div(double a, double b) throws ArithmeticException {
-        val = a / b;
         if(b != 0) {
-            display.displayValue(val);
-            return val;
+            double result = a / b;
+            display.displayValue(result);
+            return result;
         } else {
             throw new ArithmeticException("Do not divide by 0!");
         }
