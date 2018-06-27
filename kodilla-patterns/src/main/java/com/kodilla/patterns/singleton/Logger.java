125353a610b3bf59e05ada11ca7464c6e2df1d04
Mevcia <ewa.a.bronowska@gmail.com>
2018-06-27 20:56:37 +0200

    Change Logger into singleton pattern excercise 11.1 - v.2

--- kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -1,14 +1,26 @@
 package com.kodilla.patterns.singleton;
 
 public class Logger {
+    private static Logger loggerInstance = null;
     private String lastLog = "";
 
-    public void log(String log) {
-        this.lastLog = log;
-        System.out.println("Log: [" + log + "]");
+    private Logger() {
+
+    }
+
+    public static Logger getLoggerInstance() {
+        if(loggerInstance == null) {
+            loggerInstance = new Logger();
+        }
+        return loggerInstance;
     }
 
     public String getLastLog() {
         return lastLog;
+    }
+
+    public void log(String log) {
+        this.lastLog = log;
+        System.out.println("Log: [" + log + "]");
     }
 }
--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -5,19 +5,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class LoggerTestSuite {
-    private static Logger logger;
 
     @BeforeClass
     public static void log() {
-        logger = new Logger();
-        logger.log("User Mike, Thu, 2018, JUN 26th at 20:40:39");
+        Logger.getLoggerInstance().log("User Mike, Thu, 2018, JUN 26th at 20:40:39");
     }
 
     @Test
     public void testGetLastLog() {
 //        Given
 //        When
-        String lastLog = logger.getLastLog();
+        String lastLog = Logger.getLoggerInstance().getLastLog();
 //        Then
         Assert.assertEquals("User Mike, Thu, 2018, JUN 26th at 20:40:39", lastLog);
     }
